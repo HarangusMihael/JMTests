13f476b89601df62117c94cc0bf8e3a6a320ef02
Harangus Mihael <mihael.harangus@yahoo.ro>
2018-02-22 16:01:10 +0200

    Alarm test.

---
+++ XUnitTestProject1/XUnitTestProject1/AlarmTest.cs
@@ -0,0 +1,36 @@
+using System;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class AlarmTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Alarm alarm = new Alarm("Monday", 8);
+            Assert.True(SetAlarm(alarm, 8));
+        }
+
+        bool SetAlarm(Alarm Day, int hour)
+        {
+            if (Day.hour == hour)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        struct Alarm
+        {
+            public string day;
+            public int hour;
+            public Alarm(string Day, int Hour)
+            {
+                this.day = Day;
+                this.hour = Hour;
+            }
+
+        }
+    }
+}
